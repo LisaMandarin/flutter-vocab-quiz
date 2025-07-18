55f9615b3d9ac0aa8318ac4b08e132997a827f2b
Lisa <122068169+LisaMandarin@users.noreply.github.com>
2025-07-19 03:46:31 +0800

    fix: color of status bar on  top

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:lottie/lottie.dart';
 import 'package:vocab_quiz/data/vocabList.dart';
 import 'package:vocab_quiz/views/pages/practice_page.dart';
@@ -14,94 +15,97 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          Opacity(
-            opacity: 0.2,
-            child: Image.asset(
-              "assets/images/background.png",
-              fit: BoxFit.cover,
-              width: double.infinity,
-              height: double.infinity,
-            ),
-          ),
-          Center(
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Lottie.asset('assets/lotties/welcome.json'),
-                  SizedBox(height: 30),
-                  Text("Choose a set of vocab list to start"),
-                  SizedBox(height: 10),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF171717),
-                      foregroundColor: Colors.white,
-                    ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return PracticePage(
-                              title: "Adjectives",
-                              vocabList: adjList,
-                            );
-                          },
-                        ),
-                      );
-                    },
-                    child: Text("Adjectives", style: TextStyle(fontSize: 20)),
-                  ),
-                  SizedBox(height: 10),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF171717),
-                      foregroundColor: Colors.white,
-                    ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return PracticePage(
-                              title: "Verbs",
-                              vocabList: verbList,
-                            );
-                          },
-                        ),
-                      );
-                    },
-                    child: Text("Verbs", style: TextStyle(fontSize: 20)),
-                  ),
-                  SizedBox(height: 10),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF171717),
-                      foregroundColor: Colors.white,
-                    ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return PracticePage(
-                              title: "Nouns",
-                              vocabList: nounList,
-                            );
-                          },
-                        ),
-                      );
-                    },
-                    child: Text("Nouns", style: TextStyle(fontSize: 20)),
-                  ),
-                ],
+    return AnnotatedRegion(
+      value: SystemUiOverlayStyle.dark,
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Opacity(
+              opacity: 0.2,
+              child: Image.asset(
+                "assets/images/background.png",
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: double.infinity,
               ),
             ),
-          ),
-        ],
+            Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Lottie.asset('assets/lotties/welcome.json'),
+                    SizedBox(height: 30),
+                    Text("Choose a set of vocab list to start"),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF171717),
+                        foregroundColor: Colors.white,
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return PracticePage(
+                                title: "Adjectives",
+                                vocabList: adjList,
+                              );
+                            },
+                          ),
+                        );
+                      },
+                      child: Text("Adjectives", style: TextStyle(fontSize: 20)),
+                    ),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF171717),
+                        foregroundColor: Colors.white,
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return PracticePage(
+                                title: "Verbs",
+                                vocabList: verbList,
+                              );
+                            },
+                          ),
+                        );
+                      },
+                      child: Text("Verbs", style: TextStyle(fontSize: 20)),
+                    ),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF171717),
+                        foregroundColor: Colors.white,
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return PracticePage(
+                                title: "Nouns",
+                                vocabList: nounList,
+                              );
+                            },
+                          ),
+                        );
+                      },
+                      child: Text("Nouns", style: TextStyle(fontSize: 20)),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
