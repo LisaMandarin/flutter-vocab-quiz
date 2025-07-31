f5629301a3bb0c9826180ce1a28c5848e4e6b12d
Lisa <122068169+LisaMandarin@users.noreply.github.com>
2025-08-01 02:42:24 +0800

    fix: refactor setting page

---
+++ lib/views/components/usercard_widget.dart
@@ -0,0 +1,135 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:vocab_quiz/services/firestore_services.dart';
+
+class UsercardWidget extends StatefulWidget {
+  final String email;
+  final String? username;
+  final TextEditingController controllerUsername;
+  final String errorMessage;
+  final VoidCallback refresh;
+
+  const UsercardWidget({
+    super.key,
+    required this.email,
+    required this.username,
+    required this.controllerUsername,
+    required this.errorMessage,
+    required this.refresh,
+  });
+
+  @override
+  State<UsercardWidget> createState() => _UsercardWidgetState();
+}
+
+class _UsercardWidgetState extends State<UsercardWidget> {
+  Future<void> updateUsername() async {
+    final newName = widget.controllerUsername.text.trim();
+    if (newName.isEmpty) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("What's your username")));
+      return;
+    }
+    try {
+      if (widget.username == null) {
+        await firestore.value.addUsername(newName);
+      } else {
+        await firestore.value.updateUsername(newName);
+      }
+      if (!context.mounted) return;
+      Navigator.pop(context);
+      widget.refresh();
+    } on FirebaseException catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            e.message ?? "There is an error while updating username",
+          ),
+        ),
+      );
+    }
+    return;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "User",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w900),
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Icon(Icons.email_outlined),
+                  Text(": ${widget.email}"),
+                ],
+              ),
+              Row(
+                children: [
+                  Icon(Icons.person_2_outlined),
+                  Text(": ${widget.username ?? "(unknown)"}"),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          content: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              TextField(
+                                controller: widget.controllerUsername,
+                                decoration: InputDecoration(
+                                  label: Text("Username"),
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: TextButton(
+                                      onPressed: () async {
+                                        await updateUsername();
+                                      },
+                                      child: Text("Okay"),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: TextButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: Text("Cancel"),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                    child: Text("Update Username"),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/views/pages/setting_page.dart
+++ lib/views/pages/setting_page.dart
@@ -1,9 +1,9 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:vocab_quiz/data/styles.dart';
 import 'package:vocab_quiz/services/auth_services.dart';
 import 'package:vocab_quiz/services/firestore_services.dart';
 import 'package:vocab_quiz/views/components/appbar_widget.dart';
+import 'package:vocab_quiz/views/components/usercard_widget.dart';
 import 'package:vocab_quiz/views/pages/home_page.dart';
 
 class SettingPage extends StatefulWidget {
@@ -16,6 +16,9 @@ class SettingPage extends StatefulWidget {
 class _SettingPageState extends State<SettingPage> {
   final TextEditingController controllerUsername = TextEditingController();
   String errorMessage = '';
+  void refreshPage() {
+    setState(() {});
+  }
 
   @override
   void dispose() {
@@ -50,128 +53,12 @@ class _SettingPageState extends State<SettingPage> {
                     username != null ? "Hello, $username" : "Hello",
                     style: titleStyle,
                   ),
-                  Card(
-                    elevation: 10,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 20,
-                      ),
-                      child: SizedBox(
-                        width: double.infinity,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "User",
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.w900,
-                              ),
-                            ),
-                            SizedBox(height: 10),
-                            Row(
-                              children: [
-                                Icon(Icons.email_outlined),
-                                Text(": $email"),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Icon(Icons.person_2_outlined),
-                                Text(": ${username ?? "(unknown)"}"),
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                TextButton(
-                                  onPressed: () {
-                                    showDialog(
-                                      context: context,
-                                      builder: (context) => AlertDialog(
-                                        content: Column(
-                                          mainAxisSize: MainAxisSize.min,
-                                          children: [
-                                            TextField(
-                                              controller: controllerUsername,
-                                              decoration: InputDecoration(
-                                                label: Text("Username"),
-                                              ),
-                                            ),
-                                            Row(
-                                              children: [
-                                                Expanded(
-                                                  child: TextButton(
-                                                    onPressed: () async {
-                                                      setState(() {
-                                                        errorMessage = "";
-                                                      });
-                                                      if (controllerUsername
-                                                          .text
-                                                          .trim()
-                                                          .isEmpty) {
-                                                        setState(() {
-                                                          errorMessage =
-                                                              "What's your username?";
-                                                        });
-                                                        return;
-                                                      }
-                                                      try {
-                                                        if (username == null) {
-                                                          await firestore.value
-                                                              .addUsername(
-                                                                controllerUsername
-                                                                    .text
-                                                                    .trim(),
-                                                              );
-                                                        } else {
-                                                          await firestore.value
-                                                              .updateUsername(
-                                                                controllerUsername
-                                                                    .text
-                                                                    .trim(),
-                                                              );
-                                                        }
-                                                        if (!mounted) return;
-                                                        Navigator.pop(context);
-                                                        setState(() {});
-                                                      } on FirebaseException catch (
-                                                        e
-                                                      ) {
-                                                        setState(() {
-                                                          errorMessage =
-                                                              e.message ??
-                                                              "There is an error while updating username";
-                                                        });
-                                                      }
-                                                    },
-                                                    child: Text("Okay"),
-                                                  ),
-                                                ),
-                                                Expanded(
-                                                  child: TextButton(
-                                                    onPressed: () {
-                                                      Navigator.pop(context);
-                                                    },
-                                                    child: Text("Cancel"),
-                                                  ),
-                                                ),
-                                              ],
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    );
-                                  },
-                                  child: Text("Update Username"),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
+                  UsercardWidget(
+                    email: email,
+                    username: username,
+                    controllerUsername: controllerUsername,
+                    errorMessage: errorMessage,
+                    refresh: refreshPage,
                   ),
                   FilledButton(
                     style: FilledButton.styleFrom(
